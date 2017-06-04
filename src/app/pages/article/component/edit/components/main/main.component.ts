e6d55afeaf9cebb31c8a43a2b9f446815c9fa04b
huazai128 <941154174@qq.com>
2017-06-04 20:38:29 +0800

    update Article

--- src/app/pages/article/component/edit/components/main/main.component.ts
+++ src/app/pages/article/component/edit/components/main/main.component.ts
@@ -1,7 +1,7 @@
 import { Component,ViewEncapsulation,Input,Output,EventEmitter } from "@angular/core";
 import { FormBuilder,FormGroup,AbstractControl,Validators } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
-
+import { TagService } from "../../../tag/tag.server";
 
 @Component({
   selector:"edit-article",
@@ -12,13 +12,29 @@ import { ActivatedRoute } from "@angular/router";
 
 export class EditArticle{
 
+  // Input;
+  @Input() tag;
+  @Input() title;
+  @Input() content;
+  @Input() keywords;
+  @Input() description;
+  @Output() tagChange:EventEmitter<any> = new EventEmitter();
+  @Output() titleChange:EventEmitter<any> = new EventEmitter();
+  @Output() keywordsChange:EventEmitter<any> = new EventEmitter();
+  @Output() descriptionChange:EventEmitter<any> = new EventEmitter();
+
+  // Article 表单
   public editForm:FormGroup;
   public _title:AbstractControl;
   public _keywords:AbstractControl;
   public _content:AbstractControl;
   public _description:AbstractControl;
 
-  constructor(private _fb:FormBuilder){
+  // 常规参数
+  public tags:any = { data:[] }; // 用于存储tags 标签
+
+  constructor(private _fb:FormBuilder,
+              private _service:TagService){
 
     this.editForm = this._fb.group({
       "_title":["",Validators.compose([Validators.required])],
@@ -33,6 +49,21 @@ export class EditArticle{
     this._description = this.editForm.controls["_description"];
   }
 
+  //初始化
+  public ngOnInit():void{
+    this.getTags();
+  }
 
+  // 获取
+  public getTags():void{
+    this._service.getTags({ pre_page:100 })
+      .then(({result}) => {
+        // console.log(result);
+        this.tags = result;
+        console.log(this.tags);
+      })
+      .catch((err) => {});
+  }
 
+  //
 }
